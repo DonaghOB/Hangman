7bb98d9711e4cec25df5e61138c1cb706ceb87a0
Roisin Ni Bhriain <roisinnib@gmail.com>
2024-09-02 15:32:53 +0100

    Started main and setup
    Co-authored-by: Donagh <donaghobriain5@gmail.com>

--- hangman.py
+++ hangman.py
@@ -1,2 +1,11 @@
 currentAlphabet = [ 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' ]
 
+# Starting the Program
+def main():
+    
+    # SETUP
+    print( "WELCOME TO HANGMAN" )
+    
+
+if __name__ == "__main__":
+    main()
